9ec3b7f6536274d4ffc97141bd8d1d14443481fb
Jamie Yoon <rkawk3316@gmail.com>
2020-10-01 17:45:47 +0300

    Added modal closing

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
 import "./index.scss";
 import {ApiInterface, getApi} from "@navvis/indoorviewer";
 import { SideBarMenuModifier } from "./menu/SideBarMenuModifier";
-
+import { Modal } from "./modal/modal";
 class TestApp
 {
 	public ivApi: ApiInterface;
+	private modal: Modal;
 
 	constructor()
 	{
 		getApi("https://nvdev-0.iv.navvis.com/")
 			.then((iv: ApiInterface) =>
 			{
+				this.modal = new Modal();
+				this.modal.assignEventListeners();
 				this.ivApi = iv;
 				new SideBarMenuModifier(iv);
 			});
